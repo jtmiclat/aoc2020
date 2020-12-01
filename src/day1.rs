4013d946b076733ec050e2f70abc8be9656bf5f4
Jt Miclat <jtmiclat@gmail.com>
2020-12-01 16:26:40 +0800

    Finish day 1

---
+++ src/day1.rs
@@ -0,0 +1,43 @@
+use std::fs;
+
+pub fn solve_a(filename: &str) {
+    let foo: String = fs::read_to_string(filename).expect("Error opening file");
+    let numbers: Vec<i32> = foo
+        .split_whitespace()
+        .map(|n| n.parse().expect("Error parsing number"))
+        .collect();
+    for num1 in &numbers {
+        for num2 in &numbers {
+            if (num1 + num2 == 2020) && (num1 != num2) {
+                println!("Found numbers adding to 2020. {} and {}", num1, num2);
+                println!("{}", num1 * num2);
+                return;
+            }
+        }
+    }
+}
+pub fn solve_b(filename: &str) {
+    let foo: String = fs::read_to_string(filename).expect("Error opening file");
+    let numbers: Vec<i32> = foo
+        .split_whitespace()
+        .map(|n| n.parse().expect("Error parsing number"))
+        .collect();
+    for num1 in &numbers {
+        for num2 in &numbers {
+            for num3 in &numbers {
+                if (num1 + num2 + num3 == 2020)
+                    && (num1 != num2)
+                    && (num2 != num3)
+                    && (num1 != num3)
+                {
+                    println!(
+                        "Found numbers adding to 2020. {}, {}, and {}",
+                        num1, num2, num3
+                    );
+                    println!("{}", num1 * num2);
+                    return;
+                }
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use clap::clap_app;
+
+mod day1;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = clap_app!(myapp =>
+        (version: "1.0")
+        (author: "Jt Miclat <jtmiclat@gmail.com>")
+        (about: "Advent of Code 2021")
+        (@arg verbose: -v --verbose "Print test information verbosely")
+        (@subcommand day1a =>
+            (about: "Solve for the 1st problem of day 1")
+            (version: "1.0")
+            (@arg INPUT: +required "Sets the input file to use")
+        )
+        (@subcommand day1b =>
+            (about: "Solve for the 2nd problem of day 1")
+            (version: "1.0")
+            (@arg INPUT: +required "Sets the input file to use")
+        )
+    )
+    .get_matches();
+    if let Some(matches) = matches.subcommand_matches("day1a") {
+        if let Some(input) = matches.value_of("INPUT") {
+            day1::solve_a(input);
+        }
+    }
+    if let Some(matches) = matches.subcommand_matches("day1b") {
+        if let Some(input) = matches.value_of("INPUT") {
+            day1::solve_b(input);
+        }
+    }
 }
